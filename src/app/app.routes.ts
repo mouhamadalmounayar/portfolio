6cf90ba13d002b588bd24262ea795a4cd9f7e172
Mouhamad Al Mounayar <muhammad.mnayar@gmail.com>
2024-01-05 10:26:56 +0100

    bettering responsitivty

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { ProjectsComponent } from './projects/projects.component';
 import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
 
 export const routes: Routes = [
     {path: '' , component : HomeComponent},
-    {path : 'Projects' , component : ProjectsComponent}   
+    {path : 'Projects' , component : ProjectsComponent},
+    {path : 'Contact' , component : ContactComponent} 
 ];
+
