3e19084a4c1498a6989a306c501f2b1d1c7ce293
Mouhamad Al Mounayar <muhammad.mnayar@gmail.com>
2024-01-10 15:23:50 +0100

    Creating contact page

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { ContactService } from './contact.service';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
-
+  constructor(private formBuilder : FormBuilder , private contactService : ContactService){}
+  checkoutForm = this.formBuilder.group({
+    name : '',
+    email : '',
+    message : ''
+  })
+  onSubmit() : void{
+    if (this.checkoutForm.valid){
+        this.contactService.submitForm(this.checkoutForm.value).subscribe(
+          (response) => {
+            console.log(response)
+            alert(response.message)
+          }
+        )
+      }
+    }
 }
---
+++ src/app/contact/contact.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment.development';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContactService {
+  private apiUrl = environment.apiUrl
+  constructor(private http : HttpClient) {}
+  submitForm(formData : any) : Observable<any> {
+    return this.http.post(this.apiUrl + "/message" , formData)
+  }
+}
