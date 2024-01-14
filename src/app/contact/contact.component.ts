3dd92538ebe343ff3c4c715b6a5dc9955b2d5fb8
mouhamadalmounayar <muhammad.mnayar@gmail.com>
2024-01-14 15:54:11 -0500

    modifying html

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,8 @@
+
 import { Component } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { zoomOut } from '../animations/contactPageAnimations';
+
 import { ContactService } from './contact.service';
 
 @Component({
@@ -7,9 +10,14 @@ import { ContactService } from './contact.service';
   standalone: true,
   imports: [ReactiveFormsModule],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrl: './contact.component.css',
+  animations : [zoomOut]
 })
 export class ContactComponent {
+  showElement = true;
+  ngOnInit() {
+    this.showElement = false;
+  }
   constructor(private formBuilder : FormBuilder , private contactService : ContactService){}
   checkoutForm = this.formBuilder.group({
     name : '',
