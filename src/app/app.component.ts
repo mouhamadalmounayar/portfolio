646ec837272b62e9291e6778bcdc3d5f873b4201
Al Mounayar Mouhamad <132198328+mouhamadalmounayar@users.noreply.github.com>
2024-01-14 16:15:27 -0500

    Revert "Contact component"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,10 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 
-import { HomeComponent } from './home/home.component';
-import { ResumeComponent } from './resume/resume.component';
-import { ProjectsComponent } from './projects/projects.component';
-import { ScrollAnimationDirective } from './directives/scroll-animation.directive';
-
-
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, HomeComponent, ResumeComponent, ProjectsComponent, ScrollAnimationDirective],
+  imports: [CommonModule, RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,37 +1,12 @@
-
 import { Component } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
-import { zoomOut } from '../animations/contactPageAnimations';
-
-import { ContactService } from './contact.service';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css',
-  animations : [zoomOut]
+  styleUrl: './contact.component.css'
 })
 export class ContactComponent {
-  showElement = true;
-  ngOnInit() {
-    this.showElement = false;
-  }
-  constructor(private formBuilder : FormBuilder , private contactService : ContactService){}
-  checkoutForm = this.formBuilder.group({
-    name : '',
-    email : '',
-    message : ''
-  })
-  onSubmit() : void{
-    if (this.checkoutForm.valid){
-        this.contactService.submitForm(this.checkoutForm.value).subscribe(
-          (response) => {
-            console.log(response)
-            alert(response.message)
-          }
-        )
-      }
-    }
+
 }
