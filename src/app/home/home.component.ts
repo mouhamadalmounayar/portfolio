bbceeaeda1a89a8185a6dd3d693860c29babe4cc
mouhamadalmounayar <muhammad.mnayar@gmail.com>
2024-01-28 00:21:21 +0100

    minor cleanups

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { ResumeComponent } from '../resume/resume.component';
 import { ScrollAnimationDirective } from '../directives/scroll-animation.directive';
 import { FadeInDirective } from '../directives/fade-in.directive';
+import {RouterLink} from "@angular/router";
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ResumeComponent, ScrollAnimationDirective, FadeInDirective],
+  imports: [ResumeComponent, ScrollAnimationDirective, FadeInDirective, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
